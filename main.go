2923018fcc11440ed2d53b956c214e8c76d35097
engineerscodes <practicalengineers05@gmail.com>
2021-10-08 23:48:02 +0530

    added urls

--- main.go
+++ main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
+	"regexp"
 	"time"
 
 	"go.mongodb.org/mongo-driver/bson"
@@ -13,19 +16,56 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
 
+//
+type NewUser struct {
+        Name string `json:"name"`
+        Email  string    `json:"email"`
+		Time time.Time  `json:"Time"`
+}
+
 
 func Index(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprint(w, "Welcome!\n")
+        fmt.Fprint(w, "Welcome! !\n")
 }
 
 // domain/users
 func CreateUser(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprint(w, "Creating New User!\n")
+        
+	   switch 
+
+
+		fmt.Println("method:", r.Method) //get request method
+		  if r.Method == "GET" {
+		w.Header().Set("content-type","text/html")
+        t, _ := template.ParseFiles("Templates/User.html")
+		
+        t.Execute(w, nil)
+    } else {
+        r.ParseForm()
+		w.Header().Set("content-type","application/json")
+		/* for k, v := range r.Form {
+        fmt.Println("key:", k)
+        fmt.Println("val:", v)
+        }
+		*/
+        
+      /*  fmt.Println("username:", r.FormValue("name"))
+		fmt.Println("email:",r.FormValue("email"))
+        fmt.Println("password:", r.FormValue("password"))
+		*/
+		fmt.Println("Creating User ")
+     
+		var user NewUser =NewUser{Name:r.FormValue("name"),Email:r.FormValue("email"),Time:time.Now()}
+		
+		json.NewEncoder(w).Encode(user)
+
+		
+    }
 }
 
 // domain/users/<id here>
 func GetUser(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprint(w, "GET user by ID\n")
+        fmt.Fprint(w, "GET user by ID\n",r.URL)
 }
 
 
@@ -49,9 +89,8 @@ func AllUserPost(w http.ResponseWriter, r *http.Request) {
 
 
 func handleRequest(){
-	http.HandleFunc("/",Index)
+	//http.HandleFunc("/",Index)
 	http.HandleFunc("/users",CreateUser)
-	http.HandleFunc("/users/:id",GetUser)
 	http.HandleFunc("/posts",UserPost)
 	log.Fatal(http.ListenAndServe(":8001",nil))
 }
